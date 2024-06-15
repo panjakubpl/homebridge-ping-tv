6da3b9bc2df566eec48f54f950f408bfa40f99e8
JotEmCeHa <jakub@wdz.net.pl>
2024-06-15 10:53:37 +0200

    Create index.js
    Code of the index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const ping = require('ping');
+
+let Accessory, Service, Characteristic, Categories;
+
+module.exports = (api) => {
+  Accessory = api.platformAccessory;
+  Service = api.hap.Service;
+  Characteristic = api.hap.Characteristic;
+  Categories = api.hap.Categories;
+
+  api.registerAccessory('homebridge-ping-tv', 'PingTV', PingTVAccessory);
+}
+
+class PingTVAccessory {
+  constructor(log, config) {
+    this.log = log;
+    this.config = config;
+    this.name = config.name;
+    this.ipAddress = config.ipAddress;
+    this.pingInterval = config.pingInterval || 5000; // domy   ^{lnie ping co 5 sekund
+    this.category = Categories[config.category] || Categories.TELEVISION;
+
+    this.service = new Service.Television(this.name, 'Television');
+    this.setupAccessoryInformation();
+    this.setupTelevisionService();
+
+    this.checkDeviceStatus();
+  }
+
+  setupAccessoryInformation() {
+    this.informationService = new Service.AccessoryInformation()
+      .setCharacteristic(Characteristic.Manufacturer, "Custom Manufacturer")
+      .setCharacteristic(Characteristic.Model, "Custom Model")
+      .setCharacteristic(Characteristic.SerialNumber, "Custom Serial Number");
+  }
+
+  setupTelevisionService() {
+    this.service.setCharacteristic(Characteristic.ConfiguredName, this.name);
+    this.service.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
+    this.service.getCharacteristic(Characteristic.Active)
+      .on('get', this.getActiveState.bind(this));
+  }
+
+        checkDeviceStatus() {
+            setInterval(() => {
+                ping.sys.probe(this.ipAddress, (isAlive) => {
+                    const status = isAlive ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE;
+                    this.service.updateCharacteristic(Characteristic.Active, status);
+                    // Dodajemy logowanie stanu urz   ^edzenia.
+                    this.log(`Pinging IP Address ${this.ipAddress}: Device is ${isAlive ? 'active' : 'inactive'}.`);
+                });
+            }, this.pingInterval);
+        }
+
+  getActiveState(callback) {
+    ping.sys.probe(this.ipAddress, (isAlive) => {
+      const status = isAlive ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE;
+      callback(null, status);
+    });
+  }
+
+  getServices() {
+    return [this.informationService, this.service];
+  }
+}
